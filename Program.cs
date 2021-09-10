53bc1d2fee30b71b0ab78e72127caf5e4bd2b25d
Kimberley Greenbush <v-kgreenbush@microsoft.com>
2021-09-09 22:48:07 -0700

    add Phrase class

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Threading;
 
 namespace v_kgreenbush
 {
+    class Phrase
+    {
+        string text = "Screensaver";
+        int xCoord;
+        int yCoord;
+        int xDir = 1;
+        int yDir = 1;
+
+        public Phrase()
+        {
+            xCoord = 0;
+            yCoord = 0;
+        }
+
+        void MovePhrase()
+        {
+
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Phrase screensaver = new Phrase();
+
+
         }
     }
 }
